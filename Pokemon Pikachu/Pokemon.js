a1ae9f1cc8fca6fac12a7b923fc2c82863c3b10b
Jorel martins deczuta <81186935+Jojorel@users.noreply.github.com>
2021-08-27 09:37:59 -0300

    Exercicio Pikachu
    Construindo

---
+++ Pokemon Pikachu/Pokemon.js
@@ -0,0 +1,8 @@
+const evoluiComPedra = require('./Pikachu')
+
+function primeiraLetraMaiuscula(palavra) {
+    
+    const palavraSemEspaco = palavra.trim()
+    
+    return palavraSemEspaco[0].toUpperCase() + palavraSemEspaco.slice(1).toLowerCase()
+}
