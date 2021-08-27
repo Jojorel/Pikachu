2cea2ad0d7e2326f04c726c24cf46733f7d1b764
Jorel Martins <bearshamis@gmail.com>
2021-08-27 10:05:09 -0300

    mudanças

--- Pokemon Pikachu/Pokemon.js
+++ Pokemon Pikachu/Pokemon.js
@@ -1,8 +1,13 @@
-const evoluiComPedra = require('./Pikachu')
+const Pikachuu = require('./Pikachu')
 
 function primeiraLetraMaiuscula(palavra) {
     
     const palavraSemEspaco = palavra.trim()
     
     return palavraSemEspaco[0].toUpperCase() + palavraSemEspaco.slice(1).toLowerCase()
+}
+
+function imprimir(pokemon) {
+
+    
 }
